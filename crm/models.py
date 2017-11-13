19785bf5d068c9da7e6bc2f3ba8830a1452f047e
wangkai <18931890131@189.cn>
2017-11-13 12:30:36 +0800

    crm表结构框架

--- crm/models.py
+++ crm/models.py
@@ -3,4 +3,48 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    '''客户信息表'''
+    pass
+
+
+class CustomerFollowUp(models.Model):
+    '''客户跟进表'''
+    pass
+
+
+class Course(models.Model):
+    '''课程表'''
+    pass
+
+
+class ClassList(models.Model):
+    '''班级表'''
+    pass
+
+
+class CourseRecord(models.Model):
+    '''上课记录'''
+    pass
+
+
+class StudyRecord(models.Model):
+    '''学习记录'''
+    pass
+
+class Enrollment(models.Model):
+    '''报名表'''
+    pass
+
+
+class UerProfile(models.Model):
+    '''账号表'''
+    pass
+
+
+class Role(models.Model):
+    '''角色表'''
+    pass
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -35,14 +35,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 INSTALLED_APPS = [
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crm',
 ]
 
 MIDDLEWARE = [
